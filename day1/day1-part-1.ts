6f7e3dc53829bd516fd0118c1223450b0c542f60
mapolonio <matias@mapolonio.dev>
2021-12-02 12:55:41 -0300

    day 1 part 1

--- day1/day1-part-1.ts
+++ day1/day1-part-1.ts
@@ -1,7 +1,27 @@
 import { DayFunction } from '../interfaces';
+import loadInput from '../utils/load-input';
 
-const main: DayFunction = async () => {
-  return 'hello world';
+const inputParser = (inputString: string) => {
+  return inputString.split('\n').map((line) => parseInt(line, 10));
+};
+
+const countIncreasingDepths = (depths: Array<number>): number => {
+  let total = 0;
+
+  for (let i = 1; i < depths.length; i += 1) {
+    if (depths[i - 1] < depths[i]) {
+      total += 1;
+    }
+  }
+
+  return total;
+};
+
+const main: DayFunction = async (inputPath) => {
+  const depths = await loadInput(inputPath, inputParser);
+  const increasingDepths = countIncreasingDepths(depths);
+
+  return increasingDepths;
 };
 
 export default main;
