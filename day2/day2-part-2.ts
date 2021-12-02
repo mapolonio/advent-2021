ee73ad4e636747e7f55ae2750ffbbd2d4e96dc01
mapolonio <matias@mapolonio.dev>
2021-12-02 16:23:43 -0300

    day 2

---
+++ day2/day2-part-2.ts
@@ -0,0 +1,55 @@
+import { DayFunction } from '../interfaces';
+import loadInput from '../utils/load-input';
+
+interface Instruction {
+  movement: string;
+  amount: number;
+}
+
+interface Position {
+  horizontal: number;
+  depth: number;
+}
+
+const inputParser = (inputString: string) => {
+  return inputString.split('\n').map((line) => parseInstruction(line));
+};
+
+const parseInstruction = (inputLine: string): Instruction => {
+  const [instruction, amountString] = inputLine.split(' ');
+
+  return { movement: instruction, amount: parseInt(amountString, 10) };
+};
+
+const moveSubmarine = (instructions: Array<Instruction>): Position => {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  for (const instruction of instructions) {
+    const { movement, amount } = instruction;
+
+    switch (movement) {
+      case 'forward':
+        horizontal += amount;
+        depth += aim * amount;
+        break;
+      case 'down':
+        aim += amount;
+        break;
+      case 'up':
+        aim -= amount;
+    }
+  }
+
+  return { horizontal, depth };
+};
+
+const main: DayFunction = async (inputPath) => {
+  const instructions = await loadInput(inputPath, inputParser);
+  const { horizontal, depth } = moveSubmarine(instructions);
+
+  return horizontal * depth;
+};
+
+export default main;
