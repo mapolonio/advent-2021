73fa238782df512faa90ebba7022251193335cbf
mapolonio <matias@mapolonio.dev>
2021-12-02 13:41:16 -0300

    updated main script so it dynamically imports the required day file

--- index.ts
+++ index.ts
@@ -1,24 +1,17 @@
-import day1part1 from './day1/day1-part-1';
-import day1part2 from './day1/day1-part-2';
-import { DayMapping } from './interfaces';
+import { DayFunction } from './interfaces';
 const [, , scriptArg, ...options] = process.argv;
 
-const CHALLENGE_MAP: DayMapping = {
-  '1-1': day1part1,
-  '1-2': day1part2
-};
-
-const run = async () => {
+const runDay = async () => {
   const [day, part] = scriptArg.split('-');
-  const main = CHALLENGE_MAP[scriptArg];
+  const { default: main } = await import(`./day${day}/day${day}-part-${part}`);
 
   console.log(`Running Day ${day} - Part ${part}\n`);
   console.time('Runtime');
 
-  const result = await main(`day${day}/input`, options);
+  const result = await (main as DayFunction)(`day${day}/input`, options);
 
   console.log(`Result: ${result}`);
   console.timeEnd('Runtime');
 };
 
-run();
+runDay();
--- interfaces.ts
+++ interfaces.ts
@@ -2,7 +2,3 @@ export type DayFunction = (
   inputPath: string,
   options: Array<string>
 ) => Promise<any>;
-
-export interface DayMapping {
-  [index: string]: DayFunction;
-}
